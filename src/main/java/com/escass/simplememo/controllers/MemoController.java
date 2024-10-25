5c0d1fae0945e8b454329cf8f8ad80eac2ae3b97
escurse <av1967@naver.com>
2024-10-25 16:39:25 +0900

    simpleMemo

--- src/main/java/com/escass/simplememo/controllers/MemoController.java
+++ src/main/java/com/escass/simplememo/controllers/MemoController.java
@@ -1,5 +1,6 @@
 package com.escass.simplememo.controllers;
 
+import com.escass.simplememo.entities.MemoEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +18,9 @@ public class MemoController {
     }
     @RequestMapping(value = "/", method = RequestMethod.POST)
     @ResponseBody
-    public String postIndex() {
+    public String postIndex(MemoEntity memo) {
+        System.out.println(memo.getWriter());
+        System.out.println(memo.getContent());
         return "ㅋㅋㅋ";
     }
 }
