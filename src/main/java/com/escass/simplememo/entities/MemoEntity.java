95ba0315b68cab33bc0bb1771f8aaa1dc6cce43d
escurse <av1967@naver.com>
2024-10-28 09:42:37 +0900

    simpleMemo

--- src/main/java/com/escass/simplememo/entities/MemoEntity.java
+++ src/main/java/com/escass/simplememo/entities/MemoEntity.java
@@ -1,8 +1,34 @@
 package com.escass.simplememo.entities;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class MemoEntity {
+    private int index;
     private String writer;
     private String content;
+    private LocalDateTime createdAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MemoEntity that = (MemoEntity) o;
+        return index == that.index;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(index);
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
 
     public String getWriter() {
         return writer;
@@ -18,5 +44,13 @@ public class MemoEntity {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
